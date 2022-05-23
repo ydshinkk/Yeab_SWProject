75cc60e8dd105c8f0445d44a2d09440f5a4c2a92
ydshinkk <yedingshin@gmail.com>
2022-05-23 20:15:28 +0900

    Second commit

--- app/src/main/java/com/chobo/yeab_swproject/yeab_app_reserve_seat.java
+++ app/src/main/java/com/chobo/yeab_swproject/yeab_app_reserve_seat.java
@@ -2,13 +2,38 @@ package com.chobo.yeab_swproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class yeab_app_reserve_seat extends AppCompatActivity {
+
+    ImageButton Ibtn_before;
+    ImageButton Ibtn_next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_yeab_app_reserve_seat);
+
+        Ibtn_before = findViewById(R.id.Ibtn_before);
+        Ibtn_before.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(yeab_app_reserve_seat.this, yeab_app_reserve_time.class);
+                startActivity(intent);
+            }
+        });
+
+        Ibtn_next = findViewById(R.id.Ibtn_next);
+        Ibtn_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(yeab_app_reserve_seat.this, yeab_app_reserve_resCodePrint.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
